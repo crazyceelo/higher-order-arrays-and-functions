98258cb91384e06f8a61f90711ba09f98c28477a
Carlos Perez <solracias@gmail.com>
2018-09-27 20:34:47 -0700

    more fizzbuzz

--- arrayExercises/fizzBuzzDivisible.js
+++ arrayExercises/fizzBuzzDivisible.js
@@ -17,4 +17,52 @@ function fizzBuzz(number) {
   }
 }
 
-fizzBuzz(test4);
+// fizzBuzz(test4);
+
+function fizzBuzz2(number) {
+  for (let i = 1; i <= number; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+// fizzBuzz2(test4);
+
+function fizzBuzz3(number) {
+  for (let i = 1; i <= number; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+// fizzBuzz3(test4);
+
+function fizzBuzz4(number) {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+fizzBuzz4(test4);
