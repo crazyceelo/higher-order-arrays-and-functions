217be796346ae473d895a5a773906272ecda6065
Carlos Perez <solracias@gmail.com>
2020-05-20 10:15:55 -0700

    factorial practice

---
+++ arrayExercises/factorial.js
@@ -0,0 +1,13 @@
+function factorial(n){
+    if(n < 0){
+        return -1;
+    }
+    else if( n ==0){
+        return 1;
+    }
+    else{
+        return (n * factorial(n-1));
+    }
+}
+
+console.log(factorial(4));
